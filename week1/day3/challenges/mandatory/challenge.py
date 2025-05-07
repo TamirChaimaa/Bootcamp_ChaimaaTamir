3609eb325eee974af34195aba6a627715264f109
TamirChaimaa <chaimaatamir43@gmail.com>
2025-05-07 19:30:04 +0100

    Add Challenge day 3

--- week1/day3/challenges/mandatory/challenge.py
+++ week1/day3/challenges/mandatory/challenge.py
@@ -1,20 +1,49 @@
-#Daily challenge: Old MacDonald’s Farm
 class Farm:
     def __init__(self, farm_name):
-        # ... code to initialize name and animals attributes ...
+        self.name = farm_name
+        self.animals = {}
 
-    def add_animal(self, animal_type, count):
-        # ... code to add or update animal count in animals dictionary ...
+    def add_animal(self, animal_type, count=1):
+        if animal_type in self.animals:
+            self.animals[animal_type] += count
+        else:
+            self.animals[animal_type] = count
 
     def get_info(self):
-        # ... code to format animal info from animals dictionary ...
+        result = f"{self.name}'s farm\n\n"
+        for animal, count in self.animals.items():
+            result += f"{animal:<10}: {count}\n"
+        result += "\n    E-I-E-I-0!"
+        return result
+
+    def get_animal_types(self):
+        return sorted(self.animals.keys())
+
+    def get_short_info(self):
+        animal_types = self.get_animal_types()
+        animal_names = []
+
+        for animal in animal_types:
+            if self.animals[animal] > 1:
+                animal_names.append(animal + "s")
+            else:
+                animal_names.append(animal)
+
+        if len(animal_names) == 1:
+            animal_str = animal_names[0]
+        else:
+            animal_str = ", ".join(animal_names[:-1]) + " and " + animal_names[-1]
+
+        return f"{self.name}'s farm has {animal_str}."
 
 
-# Test the code 
+# Test the code
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow', 5)
 macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
-print(macdonald.get_info())
 
+print(macdonald.get_info())
+print()
+print(macdonald.get_short_info())
