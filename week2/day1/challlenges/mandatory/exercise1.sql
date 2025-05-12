fa8bb5024753a032759415359f439423f4b9fea4
TamirChaimaa <chaimaatamir43@gmail.com>
2025-05-12 15:38:07 +0100

    Day 1 week2

---
+++ week2/day1/challlenges/mandatory/exercise1.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Hollywood;
+
+CREATE TABLE actors(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ age DATE NOT NULL,
+ number_oscars SMALLINT NOT NULL
+)
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('Matt','Damon','1970-10-08', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('George','Clooney','1961-05-06', 2);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES 
+('Leonardo', 'DiCaprio', '1974-11-11', 1),
+('Tom', 'Hanks', '1956-07-09', 2),
+('Brad', 'Pitt', '1963-12-18', 2);
+
+--1. Count how many actors are in the table.
+SELECT COUNT(*) FROM actors;
+
+--2 Try to add a new actor with some blank fields. What do you think the outcome will be ?
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('', '', NULL, NULL);
+-- The outcome will be an error because the first_name and last_name fields cannot be blank or NULL due to the NOT NULL constraint.
---
+++ week2/day1/exercises/mandatory/exercise1.sql
@@ -0,0 +1,66 @@
+-- Database: public
+
+-- DROP DATABASE IF EXISTS public;
+
+CREATE DATABASE public
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+    CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price NUMERIC(10, 2)
+);
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+--1 Add the following items to the items table:
+INSERT INTO items (name, price) VALUES
+('Small Desk', 100),
+('Large Desk', 300),
+('Fan', 80);
+
+
+--2 Add 5 new customers to the customers table
+INSERT INTO customers (first_name, last_name) VALUES
+('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson');
+
+--3 Add 5 new customers to the customers table
+
+   --1 All the items
+    SELECT * FROM items;
+
+   --2 All the items with a price above 80 (80 not included).
+    SELECT * FROM items WHERE price >80;
+ 
+    --3 All the items with a price below 300. (300 included)
+    SELECT * FROM items WHERE price <= 300;
+
+    --4 All customers whose last name is ‘Smith’ (What will be your outcome?).
+    SELECT  * From customers WHERE last_name = 'Smith';
+    -- There is no customer with last name Smith, so the outcome will be an empty set.
+
+    --5 All customers whose last name is ‘Jones’.
+    SELECT * FROM customers WHERE last_name = 'Jones';
+
+    --6 All customers whose firstname is not ‘Scott’.
+    SELECT * FROM customers WHERE first_name <> 'Scott';
+
+    
+
