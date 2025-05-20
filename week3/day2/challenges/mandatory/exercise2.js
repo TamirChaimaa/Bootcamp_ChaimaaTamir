870b4140bfc3f3138cbc5570bd379fa5f07fe14b
TamirChaimaa <chaimaatamir43@gmail.com>
2025-05-20 21:49:09 +0100

    Day2 week3

--- week3/day2/challenges/mandatory/exercise2.js
+++ week3/day2/challenges/mandatory/exercise2.js
@@ -1,3 +1,4 @@
+// Single loop
 function drawPatternSingleLoop(lines) {
   let pattern = "";
   for (let i = 1; i <= lines; i++) {
@@ -8,3 +9,18 @@ function drawPatternSingleLoop(lines) {
 
 // Call the function
 drawPatternSingleLoop(6);
+
+
+// Nested Loop
+function drawPatternNestedLoops(lines) {
+  let pattern = "";
+  for (let i = 1; i <= lines; i++) {
+    for (let j = 1; j <= i; j++) {
+      pattern += "* ";
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
+}
+
+drawPatternNestedLoops(6);
