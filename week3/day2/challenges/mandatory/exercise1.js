f4f88734da430905fa9dd8d86d450cb825280fe3
TamirChaimaa <chaimaatamir43@gmail.com>
2025-05-20 21:36:31 +0100

    Day2 week3

--- week3/day2/challenges/mandatory/exercise1.js
+++ week3/day2/challenges/mandatory/exercise1.js
@@ -1,20 +1,24 @@
-let sentence = "This dinner is bad !";
-let wordNot = sentence.indexOf("not");
-console.log(wordNot);
-let wordBad = sentence.indexOf("bad");
-console.log(wordBad);
+function replaceNotBadWithGood(sentence) {
+  let wordNot = sentence.indexOf("not");
+  console.log(wordNot);
+  let wordBad = sentence.indexOf("bad");
+  console.log(wordBad);
+  let result = "";
 
-let result = "";
-
-if (wordBad > wordNot && wordNot !== -1 && wordBad !== -1) {
-    
-  result = sentence.substring(0, wordNot);
-  result = result.concat("good");
-  result = result.concat(sentence.substring(wordBad + 3));
-  console.log(result);
-}
-else{
-    console.log(sentence);
+  if (wordBad > wordNot && wordNot !== -1 && wordBad !== -1) {
+    result = sentence.substring(0, wordNot);
+    result = result.concat("good");
+    result = result.concat(sentence.substring(wordBad + 3));
+    return result;
+  } else {
+    return sentence;
+  }
 }
 
+console.log(replaceNotBadWithGood("The movie is not that bad, I like it!"));
+console.log(
+  replaceNotBadWithGood("This dinner is not that bad ! You cook well")
+);
+console.log(replaceNotBadWithGood("This movie is not so bad !"));
+console.log(replaceNotBadWithGood("This dinner is bad !"));
 
