d565c3ac1946bc9e6c210d8bfdcd4f8603d635f9
TamirChaimaa <chaimaatamir43@gmail.com>
2025-06-15 15:37:25 +0100

    Day1 week7

--- week7/exercises/my-react-app/src/App.js
+++ week7/exercises/my-react-app/src/App.js
@@ -1,29 +1,29 @@
-import "./App.css";
-import UserFavoriteAnimals from "./UserFavoritAnimals";
-import Exercise from "./Exercise3";
+// App.js
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
-// Exercise 1 & 2
 function App() {
-  const user = {
-    firstName: "Bob",
-    lastName: "Dylan",
-    favAnimals: ["Horse", "Turtle", "Elephant", "Monkey"],
-  };
-  const myelement = <h1>I Love JSX!</h1>;
-  const sum = 5 + 5;
-
   return (
-    <div>
-      {/* Exercise 1 */}
-      <p>Hello World!</p>
-      {myelement}
-      <p>React is {sum} times better with JSX</p>
-
-      {/* Exercise 2 */}
-      <h3>{user.firstName}</h3>
-      <h3>{user.lastName}</h3>
-      <UserFavoriteAnimals favAnimals={user.favAnimals} />
-       <Exercise />
+    <div style={{ width: "60%", margin: "auto", paddingTop: "50px" }}>
+      <h2>My Travel Carousel</h2>
+      <Carousel showThumbs={false} infiniteLoop autoPlay>
+        <div>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Hong_Kong_Skyline_Restitch_-_Dec_2007.jpg" alt="Hong Kong" />
+          <p className="legend">Hong Kong</p>
+        </div>
+        <div>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/7/7f/Macau_skyline_night.jpg" alt="Macao" />
+          <p className="legend">Macao</p>
+        </div>
+        <div>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Cherry_blossoms_in_Japan.jpg" alt="Japan" />
+          <p className="legend">Japan</p>
+        </div>
+        <div>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Las_Vegas_Montage_2013.jpg" alt="Las Vegas" />
+          <p className="legend">Las Vegas</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
